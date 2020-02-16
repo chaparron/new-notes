879b4dad3a4701cfa5304d9cdfb22546054b24b4
Chaparron <chaparron@github.com>
2020-02-17 00:57:20 +0100

    bbdd configurada, ahora tocara video 5

---
+++ src/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+    .then(db => console.log('database is connected'))
+    .catch(e => console.log(err));
