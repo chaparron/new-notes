a1600fa1531a2cf7a00affb7ad5cc917686aaf07
Chaparron <chaparron@github.com>
2020-02-17 00:41:29 +0100

    configuramos el server y el index minimamente, video 3 acabado, tocara el 5

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+
+//initializations
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views' ))
+
+//Middlewares
+app.use(express.urlencoded({extended:false}));
+
+//Global variables
+
+//Routes
+app.get('/', (req,res) =>{
+    res.send('Hello world');
+})
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+module.exports = app;
+
