f3c95f98a55bceac4e02fc6e8f42903f05ac64b8
Chaparron <chaparron@github.com>
2020-02-17 01:44:26 +0100

    handlebars configurado, ahora tocara video 7

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
-
+const exphbs = require('express-handlebars');
 
 //initializations
 const app = express();
 
 //Settings
 app.set('port', process.env.PORT || 3000)
-app.set('views', path.join(__dirname, 'views' ))
-
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs')
 //Middlewares
 app.use(express.urlencoded({extended:false}));
 
@@ -16,7 +22,7 @@ app.use(express.urlencoded({extended:false}));
 
 //Routes
 app.get('/', (req,res) =>{
-    res.send('Hello world');
+    res.render('index');
 })
 
 //Static files
