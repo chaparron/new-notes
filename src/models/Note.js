4f6eef4a7660dd6963fd8ca5a6cf0d70273d9fd5
Chaparron <chaparron@github.com>
2020-02-17 01:20:34 +0100

    modelos de notas y usuarios definidos, video 5 acabado

---
+++ src/models/Note.js
@@ -0,0 +1,16 @@
+const {Schema, model} = require('mongoose');
+
+const NoteSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+},
+    {timestamps: true} //Crea los campos de creado y editado automáticamente
+)
+
+module.exports = model('Note', NoteSchema);
---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const {Schema, model} = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new Schema({
+    name:{type:String, required: true},
+    email:{type:String, required: true},
+    password:{type:String, required: true}
+
+},
+{timestamps: true}
+);
+//ciframos la contraseña
+UserSchema.methods.encryptPassword = async password =>{ //encryptPassword es el nombre que le damos nosotros a la función
+    const salt = await bcrypt.genSalt(10)
+    return await bcrypt.hash(password, salt);
+}
+//devuelve true si la contraseña o false si es incorrecta
+UserSchema.methods.matchPassword = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+module.exports = model('User', UserSchema)
