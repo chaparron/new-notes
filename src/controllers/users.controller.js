863251daa30952de1fc21e76811aee235c93bc34
Chaparron <chaparron@github.com>
2020-02-21 02:24:16 +0100

    video 16 completado. Registro de usuarios funcionando con sistema de errores que te avisa cuando la contraseña es muy corta, no coinciden los pass... hay que revisar el req.flash si queremos dejar los campos rellenados cuando hay error

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,10 +1,47 @@
 const usersCtrl = {};
+const User = require('../models/User');
 
 usersCtrl.renderSignUpForm = (req,res) => {
     res.render('users/signup')
 }
-usersCtrl.signUp = (req,res) => {
-    res.send('signup')
+usersCtrl.signUp = async (req,res) => {
+    const errors = [];
+    const { name, email, password, confirm_password } = req.body;
+    if (password != confirm_password){
+        errors.push({ text: 'passwords do not match' });
+    }
+    if (password.length < 4){
+        errors.push({ text: 'password must have more than 4 characters'})
+    }
+    if ( errors.length > 0 ){
+        res.render('users/signup', 
+        { errors,
+        name,
+        email,
+        password,
+        confirm_password }
+        )
+    } else {
+        const email_user = await User.findOne({email: email});
+        if (email_user) {
+            //Creo un nuevo error para mantener los campos, Fazt lo hace con flash
+            //req.flash("error_msg", "The Email is already in use.");
+            //res.redirect("/users/signup");
+            error_msg = "The Email is already in use."
+            res.render('users/signup', 
+            { error_msg,
+            name,
+            email,
+            password,
+            confirm_password })
+        } else {
+            const newUser = new User({ name, email, password })
+            newUser.password = await newUser.encryptPassword(password)
+            await newUser.save();
+            req.flash('success_msg', 'Usuario guardado correctamente')
+            res.redirect('/users/signin')
+        }
+    }
 }
 usersCtrl.renderSignInForm = (req,res) => {
     res.render('users/signin')
--- src/database.js
+++ src/database.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI,{
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useCreateIndex: true
 })
     .then(db => console.log('database is connected'))
     .catch(e => console.log(err));
